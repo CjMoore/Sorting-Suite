0fbf9bf90d7cedadd720874f7bb4824f49b5fad6
CJ <mooc123@gmail.com>
2016-12-02 07:54:37 -0700

    Update insertion_sort.rb

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,20 +1,13 @@
 
-
-# arr = [3, 6, 4, 9, 7, 1]
-
-
-def insertion_sort!(to_be_sorted)
- (to_be_sorted.count).times do |i|
-    temp = to_be_sorted[i]
-    k = i
-    while k > 0 && temp < to_be_sorted[k - 1]
-        to_be_sorted[k] = to_be_sorted[k - 1]
-        k -= 1
-        to_be_sorted[k] = temp
+def insertion_sort(to_be_sorted)
+  (to_be_sorted.count).times do |index|
+    temporary = to_be_sorted[index]
+    counter = index
+    while index > 0 && temp < to_be_sorted[counter - 1]
+        to_be_sorted[counter] = to_be_sorted[counter - 1]
+        counter -= 1
+        to_be_sorted[counter] = temporary
     end
- end
+  end
 end
-
-# insertion_sort!(arr)
-# puts arr
 
